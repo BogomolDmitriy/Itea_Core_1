4965a36a08e0200a21f5da79398d58a068085e2e
BogomolDmitriy <bogomoldima1992@gmail.com>
2022-07-01 22:00:35 +0300

    Add project files.

---
+++ Itea_Core_1/Bookshelf.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Itea_Core_1
+{
+    public class Bookshelf
+    {
+        public string bookGenre { get; set; }
+        public string author { get; set; }
+        public string bookTitle { get; set; }
+        public int writingYear { get; set; }
+        public int page—ount { get; set; }
+    }
+}
---
+++ Itea_Core_1/Controllers/BookshelfController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Itea_Core_1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookshelfController : ControllerBase
+    {
+        private static List<Bookshelf> Bookshelf { get; set; }
+
+        static BookshelfController() 
+        {
+            Bookshelf = new List<Bookshelf>();
+        }
+
+        private readonly ILogger<BookshelfController> _logger;
+
+        public BookshelfController(ILogger<BookshelfController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public void Create (Bookshelf books)
+        {
+            Bookshelf.Add(books);
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, Bookshelf books)
+        {
+            Bookshelf[id] = books;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete (int id)
+        {
+            Bookshelf.Remove(Bookshelf[id]);
+        }
+
+        [HttpGet]
+        public IEnumerable<Bookshelf> Get()
+        {
+            return Bookshelf;
+        }
+
+        [HttpGet("{id}")]
+        public Bookshelf GetByID(int id)
+        {
+            return Bookshelf[id];
+        }
+    }
+}
